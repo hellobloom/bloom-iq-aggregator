1d503eac3793de1b99560cc4d961edb03808e74c
djvs <djvansch@gmail.com>
2019-07-01 17:38:27 -0400

    fix

--- src/api/ctrl/subjects.ts
+++ src/api/ctrl/subjects.ts
@@ -3,6 +3,10 @@ import { renderError } from "@src/api/renderError"
 import { TApiRoutes } from "@src/types/api/basetypes"
 import * as T from "@src/types/api/subjects"
 
+const post = async (req: T.post.req): Promise<T.post.res> => {
+  return { status: 200, json: { success: true } }
+}
+
 const create = async (req: T.create.req): Promise<T.create.res> => {
   return { status: 200, json: { success: true } }
 }
@@ -12,6 +16,11 @@ const del = async (req: T.del.req): Promise<T.del.res> => {
 }
 
 const routes: Array<TApiRoutes<any, any>> = [
+  {
+    method: "post",
+    paths: "/api/v1/:subjectAddr/association",
+    fn: create
+  },
   {
     method: "post",
     paths: "/api/v1/:subjectAddr/association",
--- src/types/sigs.ts
+++ src/types/sigs.ts
@@ -1,16 +1,19 @@
 import { AttestationData as AD } from "@bloomprotocol/attestations-lib"
 export type TDate = string
+export type TUuid = string
+export type TAddr = string
+export type TSha256 = string
 
 export interface allowAssociationStr {
   type: "bloom-iq-allow-association"
   timestamp: TDate
-  aggregator: "0x..."
+  aggregator: TAddr
 }
 
 export interface revokeAssociationStr {
   type: "bloom-iq-revoke-association"
   timestamp: TDate
-  aggregator: string // "0x..."
+  aggregator: TAddr
 }
 
 export interface listReporterStr {
@@ -21,39 +24,34 @@ export interface listReporterStr {
 export interface allowReporterStr {
   type: "bloom-iq-allow-reporter"
   timestamp: TDate
-  reporterAddr: "0x..."
+  reporterAddr: TAddr
 }
 
 export interface revokeReporterStr {
   type: "bloom-iq-revoke-reporter"
-
   timestamp: TDate
-
-  reporterAddr: "0x..."
+  reporterAddr: TAddr
 }
 
 export interface submitReportStr {
   type: "bloom-iq-submit-report"
   timestamp: TDate
-  subjectAddr: string
+  subjectAddr: TAddr
   report: reportStr<AD.IBaseAttUtility | AD.IBaseAttLoan> //, encrypted for subject public key),
-  reportSha: string // sha256 of report
+  reportSha: TSha256
 }
 
 export interface revokeReportStr {
   type: "bloom-iq-revoke-report"
-
   timestamp: TDate
-
-  reportId: "..."
-
-  subjectAddr: "..."
+  reportId: TUuid
+  subjectAddr: TAddr
 }
 
 export interface performAttestationStr {
   type: "bloom-iq-perform-attestation"
   timestamp: TDate
-  aggregatorAddr: "0x..."
+  aggregatorAddr: TAddr
 }
 
 export interface reportStr<AttestationType> {
